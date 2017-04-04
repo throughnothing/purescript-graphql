2716c30e54998f95a3aeb8e6e22dda9ca8ea2120
William Wolf <throughnothing@gmail.com>
2017-04-04 11:16:56 -0700

    Make test file run

--- test/Main.purs
+++ test/Main.purs
@@ -1,4 +1,5 @@
-module Main where
+module Test.Main where
+
 import Prelude
 import GraphQL.Language.Parser as P
 import Text.Parsing.StringParser.String as S
@@ -14,42 +15,43 @@ main = do
   log $ show $ runParser ((P.name <* P.but (S.string "!")) `P.tryAlt` S.string "te") "test!"
 
   log "selection:"
-  log $ show $ runParser P.selection selection
+  log $ show $ runParser P.selection D.selection
 
   log "selectionSet:"
-  log $ show $ runParser P.selectionSet selectionSet
+  log $ show $ runParser P.selectionSet D.selectionSet
 
   log "field:"
-  log $ show $ runParser P.field aliasField
-  log $ show $ runParser P.field field
-  log $ show $ runParser P.field field2
-  log $ show $ runParser P.field field3
-  log $ show $ runParser P.field field4
+  log $ show $ runParser P.field D.fieldWithAlias
+  log $ show $ runParser P.field D.field1
+  log $ show $ runParser P.field D.field2
+  log $ show $ runParser P.field D.field3
+  log $ show $ runParser P.field D.field4
 
   log "arguments:"
-  log $ show $ runParser P.arguments arguments
-  log $ show $ runParser ((\s a -> Tuple s a) <$> P.name <*> P.arguments) nameArguments
+  log $ show $ runParser P.arguments D.arguments1
+  log $ show $ runParser P.arguments D.arguments2
+  log $ show $ runParser ((\s a -> Tuple s a) <$> P.name <*> P.arguments) D.nameArguments
 
   log "argument:"
-  log $ show $ runParser P.argument argument
+  log $ show $ runParser P.argument D.argument
 
   log "operationType:"
-  log $ show $ runParser P.operationType testOperationTypeQ
-  log $ show $ runParser P.operationType testOperationTypeM
+  log $ show $ runParser P.operationType D.operationTypeQuery
+  log $ show $ runParser P.operationType D.operationTypeMutation
 
   log "variableDefinition:"
-  log $ show $ runParser P.variableDefinition variableDefinition
+  log $ show $ runParser P.variableDefinition D.variableDefinition
 
   log "variableDefinitions:"
-  log $ show $ runParser P.variableDefinitions variableDefinitions
+  log $ show $ runParser P.variableDefinitions D.variableDefinitions
 
   log "operationDefinition:"
-  log $ show $ runParser P.operationDefinition operationDefinition
+  log $ show $ runParser P.operationDefinition D.operationDefinition
 
   log "document:"
-  log $ show $ runParser P.document swsimple
-  log $ show $ runParser P.document swSimple2
-  log $ show $ runParser P.document kitchenSinkNoComments
-  log $ show $ runParser P.document kitchenSink
+  log $ show $ runParser P.document D.swsimple
+  log $ show $ runParser P.document D.swSimple2
+  log $ show $ runParser P.document D.kitchenSinkNoComments
+  log $ show $ runParser P.document D.kitchenSink
 
 
