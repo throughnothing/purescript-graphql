0bd2cf185ee9ef501cbe45aa16372a1c5bcc5abc
William Wolf <throughnothing@gmail.com>
2017-04-04 11:14:12 -0700

    Remove Main.purs

--- src/Main.purs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (log, CONSOLE)
-
-main :: ∀ e. Eff (console :: CONSOLE | e) Unit
-main = log "Hello, Sailor!"
